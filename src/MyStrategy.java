12eeb10447a04e5698a74585e9f9e1041fcde60f
Hohol <nikita.glashenko@gmail.com>
2019-11-28 20:08:37 +0400

    collect health packs

--- src/MyStrategy.java
+++ src/MyStrategy.java
@@ -1,58 +1,95 @@
 import model.*;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MyStrategy {
-    static double distanceSqr(Vec2Double a, Vec2Double b) {
-        return (a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY());
+
+    Unit me;
+    Game game;
+
+    public UnitAction getAction(Unit me, Game game, Debug debug) {
+        this.me = me;
+        this.game = game;
+        Unit enemy = chooseEnemy();
+        LootBox targetBonus = chooseTargetBonus();
+        Vec2Double targetPos = chooseTarget(enemy, targetBonus);
+        Vec2Double aimDir = aim(enemy);
+        boolean jump = shouldJump(targetPos);
+        double velocity = getVelocity(targetPos);
+        return new UnitAction(velocity, jump, !jump, aimDir, true, false, false);
     }
 
-    public UnitAction getAction(Unit unit, Game game, Debug debug) {
+    private double getVelocity(Vec2Double targetPos) {
+        double myX = me.getPosition().getX();
+        if (Math.abs(myX - targetPos.getX()) <= me.getSize().getX() / 8) {
+            return 0;
+        }
+        return (targetPos.getX() > myX) ?
+                game.getProperties().getUnitMaxHorizontalSpeed() :
+                -game.getProperties().getUnitMaxHorizontalSpeed();
+    }
+
+    private boolean shouldJump(Vec2Double targetPos) {
+        boolean jump = targetPos.getY() > me.getPosition().getY();
+        if (targetPos.getX() > me.getPosition().getX() && game.getLevel()
+                .getTiles()[(int) (me.getPosition().getX() + 1)][(int) (me.getPosition().getY())] == Tile.WALL) {
+            jump = true;
+        }
+        if (targetPos.getX() < me.getPosition().getX() && game.getLevel()
+                .getTiles()[(int) (me.getPosition().getX() - 1)][(int) (me.getPosition().getY())] == Tile.WALL) {
+            jump = true;
+        }
+        return jump;
+    }
+
+    private Vec2Double aim(Unit enemy) {
+        return new Vec2Double(
+                enemy.getPosition().getX() - me.getPosition().getX(),
+                enemy.getPosition().getY() - me.getPosition().getY()
+        );
+    }
+
+    private Vec2Double chooseTarget(Unit nearestEnemy, LootBox targetBonus) {
+        Vec2Double targetPos = me.getPosition();
+        if (targetBonus != null && targetBonus.getItem() instanceof Item.HealthPack) {
+            targetPos = targetBonus.getPosition();
+        } else if (targetBonus != null && me.getWeapon() == null) {
+            targetPos = targetBonus.getPosition();
+        } else if (nearestEnemy != null) {
+            targetPos = nearestEnemy.getPosition();
+        }
+        return targetPos;
+    }
+
+    private LootBox chooseTargetBonus() {
+        Map<Class<? extends Item>, List<LootBox>> map = Stream.of(game.getLootBoxes())
+                .collect(Collectors.groupingBy(b -> b.getItem().getClass()));
+        List<LootBox> weapons = map.getOrDefault(Item.Weapon.class, Collections.emptyList());
+        List<LootBox> healthPacks = map.getOrDefault(Item.HealthPack.class, Collections.emptyList());
+        List<LootBox> mines = map.getOrDefault(Item.Mine.class, Collections.emptyList());
+
+        List<LootBox> required = me.getWeapon() == null ? weapons : healthPacks;
+        return required.stream()
+                .min(Comparator.comparing(lb -> sqrDist(lb.getPosition(), me.getPosition())))
+                .orElse(null);
+    }
+
+    private Unit chooseEnemy() {
         Unit nearestEnemy = null;
         for (Unit other : game.getUnits()) {
-            if (other.getPlayerId() != unit.getPlayerId()) {
-                if (nearestEnemy == null || distanceSqr(unit.getPosition(),
-                        other.getPosition()) < distanceSqr(unit.getPosition(), nearestEnemy.getPosition())) {
+            if (other.getPlayerId() != me.getPlayerId()) {
+                if (nearestEnemy == null || sqrDist(me.getPosition(),
+                        other.getPosition()) < sqrDist(me.getPosition(), nearestEnemy.getPosition())) {
                     nearestEnemy = other;
                 }
             }
         }
-        LootBox nearestWeapon = null;
-        for (LootBox lootBox : game.getLootBoxes()) {
-            if (lootBox.getItem() instanceof Item.Weapon) {
-                if (nearestWeapon == null || distanceSqr(unit.getPosition(),
-                        lootBox.getPosition()) < distanceSqr(unit.getPosition(), nearestWeapon.getPosition())) {
-                    nearestWeapon = lootBox;
-                }
-            }
-        }
-        Vec2Double targetPos = unit.getPosition();
-        if (unit.getWeapon() == null && nearestWeapon != null) {
-            targetPos = nearestWeapon.getPosition();
-        } else if (nearestEnemy != null) {
-            targetPos = nearestEnemy.getPosition();
-        }
-        debug.draw(new CustomData.Log("Target pos: " + targetPos));
-        Vec2Double aim = new Vec2Double(0, 0);
-        if (nearestEnemy != null) {
-            aim = new Vec2Double(nearestEnemy.getPosition().getX() - unit.getPosition().getX(),
-                    nearestEnemy.getPosition().getY() - unit.getPosition().getY());
-        }
-        boolean jump = targetPos.getY() > unit.getPosition().getY();
-        if (targetPos.getX() > unit.getPosition().getX() && game.getLevel()
-                .getTiles()[(int) (unit.getPosition().getX() + 1)][(int) (unit.getPosition().getY())] == Tile.WALL) {
-            jump = true;
-        }
-        if (targetPos.getX() < unit.getPosition().getX() && game.getLevel()
-                .getTiles()[(int) (unit.getPosition().getX() - 1)][(int) (unit.getPosition().getY())] == Tile.WALL) {
-            jump = true;
-        }
-        UnitAction action = new UnitAction();
-        action.setVelocity(targetPos.getX() - unit.getPosition().getX());
-        action.setJump(jump);
-        action.setJumpDown(!jump);
-        action.setAim(aim);
-        action.setShoot(true);
-        action.setSwapWeapon(false);
-        action.setPlantMine(false);
-        return action;
+        return nearestEnemy;
+    }
+
+    static double sqrDist(Vec2Double a, Vec2Double b) {
+        return (a.getX() - b.getX()) * (a.getX() - b.getX()) + (a.getY() - b.getY()) * (a.getY() - b.getY());
     }
 }
